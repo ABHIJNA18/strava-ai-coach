6c39f811d368425c39168054628a9f0c46abe6f7
Abhijna <aabhijnak@gmail.com>
2026-07-07 16:34:35 +0200

    Built query layer with stats endpoints and handlers

--- internal/database/models.go
+++ internal/database/models.go
@@ -2,6 +2,8 @@ package database
 
 import "time"
 
+//tags are needed as APIs usually return: api formats like "total_distance" instead of TotalDistance
+
 type Athlete struct {
 	ID               int64
 	StravaAthleteID int64
@@ -20,43 +22,57 @@ type OAuthToken struct {
 }
 
 type Activity struct {
-	ID                     int64
+	ID               int64     `json:"id"`
+	StravaActivityID int64     `json:"strava_activity_id"`
+	AthleteID        int64     `json:"athlete_id"`
 
-	StravaActivityID       int64
+	Name             string    `json:"name"`
+	Type             string    `json:"type"`
+	SportType        string    `json:"sport_type"`
 
-	AthleteID              int64
+	Distance         float64   `json:"distance"`
+	MovingTime       int       `json:"moving_time"`
+	ElapsedTime      int       `json:"elapsed_time"`
 
-	Name                   string
-	Type                   string
-	SportType              string
+	TotalElevationGain float64 `json:"total_elevation_gain"`
 
-	Distance               float64
+	AverageSpeed       float64 `json:"average_speed"`
+	MaxSpeed           float64 `json:"max_speed"`
 
-	MovingTime             int
-	ElapsedTime            int
+	AverageHeartrate   float64 `json:"average_heartrate"`
+	MaxHeartrate       float64 `json:"max_heartrate"`
 
-	TotalElevationGain     float64
+	AverageCadence     float64 `json:"average_cadence"`
 
-	AverageSpeed           float64
-	MaxSpeed               float64
+	AverageWatts       float64 `json:"average_watts"`
+	MaxWatts           float64 `json:"max_watts"`
 
-	AverageHeartrate       float64
-	MaxHeartrate           float64
+	WeightedAverageWatts float64 `json:"weighted_average_watts"`
 
-	AverageCadence         float64
+	Kilojoules         float64 `json:"kilojoules"`
+	SufferScore        float64 `json:"suffer_score"`
 
-	AverageWatts           float64
-	MaxWatts               float64
-	WeightedAverageWatts   float64
+	DeviceName         string `json:"device_name"`
 
-	Kilojoules             float64
+	StartDate          time.Time `json:"start_date"`
+	StartDateLocal     time.Time `json:"start_date_local"`
 
-	SufferScore            float64
+	CreatedAt          time.Time `json:"created_at"`
+}
 
-	DeviceName             string
+type ActivityStats struct {
+	TotalActivities     int     `json:"total_activities"`
+	TotalRuns           int     `json:"total_runs"`
+	TotalHikes          int     `json:"total_hikes"`
+	TotalWeightTraining int     `json:"total_weight_training"`
 
-	StartDate              time.Time
-	StartDateLocal         time.Time
+	TotalDistance       float64 `json:"total_distance"`
+	TotalRunDistance    float64 `json:"total_run_distance"`
+	TotalHikeDistance   float64 `json:"total_hike_distance"`
 
-	CreatedAt              time.Time
+	AverageHeartRate    float64 `json:"average_heart_rate"`
+	AverageRunHeartRate float64 `json:"average_run_heart_rate"`
+
+	LongestRunDistance  float64 `json:"longest_run_distance"`
+	LongestHikeDistance float64 `json:"longest_hike_distance"`
 }
